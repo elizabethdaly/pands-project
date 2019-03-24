d9abab1618040e8f5ba6eca72a024aeb092b5ea9
elizabethdaly <liz.m.a.daly@gmail.com>
2019-03-24 19:32:01 +0000

    Read in the data

---
+++ get-data.py
@@ -0,0 +1,23 @@
+# Script to read in iris data
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Read the csv file from this directory.
+data = pd.read_csv('iris.csv')
+
+# data.index
+
+# Find the column labels.
+labels = data.columns
+print(labels)
+
+# Separate out the columns according to their indices.
+slen = data['SepalLength']
+swid = data['SepalWidth']
+plen = data['PetalLength']
+pwid = data['PetalWidth']
+
+print(slen)
+# Plot them.
+
