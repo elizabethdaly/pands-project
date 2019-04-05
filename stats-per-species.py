f6c936635ba81179a6722f2acbfcab9ebdd593c4
elizabethdaly <liz.m.a.daly@gmail.com>
2019-04-05 13:05:03 +0100

    Stats per species with plots

--- stats-per-species.py
+++ stats-per-species.py
@@ -24,14 +24,54 @@ print("Data column labels: ", col_labels)
 
 # ###########################################################
 # Create 3 dataframes, each corresponding to a single species.
+# Select rows based on species names using .iloc[]
 setosa = data.loc[data['Name'] == "Iris-setosa"]
 versicolor = data.loc[data['Name'] == "Iris-versicolor"]
 virginica = data.loc[data['Name'] == "Iris-virginica"]
 
-# Check them.
-print("size=", setosa.shape)
-print(setosa.head())
-print("size=", versicolor.shape)
-print(versicolor.head())
-print("size=", virginica.shape)
-print(virginica.head())
+# # Check them.
+# print("size=", setosa.shape)
+# print(setosa.head())
+# print("size=", versicolor.shape)
+# print(versicolor.head())
+# print("size=", virginica.shape)
+# print(virginica.head())
+
+# Summary statistics for Iris-setosa.
+setosa_summary = setosa.describe()
+print("Iris-setosa")
+print(setosa_summary)
+
+# Bar plot with some of the statistics referenced by name.
+setosa_summary.loc[['mean', 'std', '50%']].plot.bar()
+plt.title("Summary statistics: Setosa", fontsize=18)
+plt.ylabel('cm', fontsize=16)
+plt.grid()
+plt.savefig('SetosaStats.jpeg')
+plt.show()
+
+# Summary statistics for Iris-versicolor.
+versicolor_summary = versicolor.describe()
+print("Iris-versicolor")
+print(versicolor_summary)
+
+# Bar plot with some of the statistics referenced by name.
+versicolor_summary.loc[['mean', 'std', '50%']].plot.bar()
+plt.title("Summary statistics: Versicolor", fontsize=18)
+plt.ylabel('cm', fontsize=16)
+plt.grid()
+plt.savefig('VersicolorStats.jpeg')
+plt.show()
+
+# Summary statistics for Iris-.virginica.
+virginica_summary = virginica.describe()
+print("Iris-virginica")
+print(virginica_summary)
+
+# Bar plot with some of the statistics referenced by name.
+virginica_summary.loc[['mean', 'std', '50%']].plot.bar()
+plt.title("Summary statistics: Virginica", fontsize=18)
+plt.ylabel('cm', fontsize=16)
+plt.grid()
+plt.savefig('VirginicaStats.jpeg')
+plt.show()
