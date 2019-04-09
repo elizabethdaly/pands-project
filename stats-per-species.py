ed7191fea4c8e84a07bed93539f7fc0f60328233
elizabethdaly <liz.m.a.daly@gmail.com>
2019-04-09 17:33:14 +0100

    Add code to generate boxplots

--- stats-per-species.py
+++ stats-per-species.py
@@ -64,7 +64,7 @@ plt.grid()
 plt.savefig('VersicolorStats.jpeg')
 plt.show()
 
-# Summary statistics for Iris-.virginica.
+# Summary statistics for Iris-virginica.
 virginica_summary = virginica.describe()
 print("Iris-virginica")
 print(virginica_summary)
@@ -79,15 +79,18 @@ plt.show()
 
 # ###########################################################
 # Can also look at some statistics per species using Pandas groupby()
+# Use rot keyword in plot() to align x-tick labels nicely.
 
 # Mean
 print(data.groupby('Name')['SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth'].mean())
 
 # Bar chart of mean values for each species. 
-data.groupby('Name')['SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth'].mean().plot(kind='bar')
-plt.title('Mean variable values')
+data.groupby('Name')['SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth'].mean(). \
+plot(kind='bar',rot='horizontal')
+plt.title('Mean variable values', fontsize=18)
 plt.ylabel('cm', fontsize=16)
 plt.grid()
+plt.legend(loc='best', fontsize=12)
 plt.savefig('Mean_species.jpeg')
 plt.show()
 
@@ -95,33 +98,44 @@ plt.show()
 print(data.groupby('Name')['SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth'].std())
 
 # Bar chart of std values for each species. 
-data.groupby('Name')['SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth'].std().plot(kind='bar')
-plt.title('Standard deviation of variable values')
+data.groupby('Name')['SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth'].std(). \
+plot(kind='bar', rot='horizontal')
+plt.title('Standard deviation of variable values', fontsize=18)
 plt.ylabel('cm', fontsize=16)
 plt.grid()
+plt.legend(loc='best', fontsize=12)
 plt.savefig('Std_species.jpeg')
 plt.show()
 
 # ###########################################################
-# Or, I could do almost all of above visually using Matplotlib box and whisker plot
+# Or, I could do almost all of above visually using Matplotlib 
+# box and whisker plot:
+# The box extends from the lower to upper quartile values of the data.
+# There is a a line at the median. 
+# The whiskers extend from the box to show the range of the data. 
+# Outlying points are those past the end of the whiskers.
+# Place all 3 box plots in same figure using subplot() - too messy.
 
 # Setosa
+# plt.subplot(3,1,1)
 setosa.boxplot()
-plt.title("Setosa boxplot", fontsize=18)
+plt.title("Iris-Setosa", fontsize=18)
 plt.ylabel('cm', fontsize=16)
-plt.savefig('Setoasa_boxplot.jpeg')
+plt.savefig('Setosa_boxplot.jpeg')
 plt.show()
 
 # Versicolor
+# plt.subplot(3,1,2)
 versicolor.boxplot()
-plt.title("Versicolor boxplot", fontsize=18)
+plt.title("Iris-Versicolor", fontsize=18)
 plt.ylabel('cm', fontsize=16)
 plt.savefig('Versicolor_boxplot.jpeg')
 plt.show()
 
 # Virginica
+# plt.subplot(3,1,3)
 virginica.boxplot()
-plt.title("Virginica boxplot", fontsize=18)
+plt.title("Iris-Virginica", fontsize=18)
 plt.ylabel('cm', fontsize=16)
 plt.savefig('Virginica_boxplot.jpeg')
 plt.show()
