57876c872c1bf011e741e6a1c28a298f809c73ea
elizabethdaly <liz.m.a.daly@gmail.com>
2019-04-12 16:24:15 +0100

    Fix up histograms subplot

--- class-separation.py
+++ class-separation.py
@@ -28,7 +28,7 @@ print("Data column labels: ", col_labels)
 species = data.Name.unique()
 print("The three species are: ", species)
 
-# Histogram of SepalLength for all species 
+# Histogram of SepalLength for all species.
 # There's only one legend, so I have to get the 3 species names into it in legend handle.
 data.groupby('Name')['SepalLength'].hist(bins=10, alpha=0.5, stacked=True)
 plt.title('SepalLength', fontsize=18)
@@ -38,7 +38,7 @@ plt.legend((species), loc='best', fontsize=12)
 plt.savefig('Hist_SepalLength.jpeg')
 plt.show()
 
-# Histogram of SepalWidth for all species 
+# Histogram of SepalWidth for all species.
 data.groupby('Name')['SepalWidth'].hist(bins=10, alpha=0.5, stacked=True)
 plt.title('SepalWidth', fontsize=18)
 plt.xlabel('cm', fontsize=16)
@@ -47,7 +47,7 @@ plt.legend((species), loc='best', fontsize=12)
 plt.savefig('Hist_SepalWidth.jpeg')
 plt.show()
 
-# Histogram of PetalLength for all species 
+# Histogram of PetalLength for all species.
 data.groupby('Name')['PetalLength'].hist(bins=10, alpha=0.5, stacked=True)
 plt.title('PetalLength', fontsize=18)
 plt.xlabel('cm', fontsize=16)
@@ -56,7 +56,7 @@ plt.legend((species), loc='best', fontsize=12)
 plt.savefig('Hist_PetalLength.jpeg')
 plt.show()
 
-# Histogram of PetalWidth for all species 
+# Histogram of PetalWidth for all species.
 data.groupby('Name')['PetalWidth'].hist(bins=10, alpha=0.5, stacked=True)
 plt.title('PetalWidth', fontsize=18)
 plt.xlabel('cm', fontsize=16)
@@ -65,7 +65,43 @@ plt.legend((species), loc='best', fontsize=12)
 plt.savefig('Hist_PetalWidth.jpeg')
 plt.show()
 
+# Plot all four histograms in one figure using subplot.
+# Use a shorter legend for these small graphs maybe.
+plt.subplot(2,2,1)
+data.groupby('Name')['SepalLength'].hist(bins=10, alpha=0.5, stacked=True)
+plt.title('SepalLength', fontsize=12)
+#plt.xlabel('cm', fontsize=10)
+plt.ylabel('Count', fontsize=10)
+plt.legend((species), loc='best', fontsize=10, fancybox=True, framealpha=0.5)
 
+plt.subplot(2,2,2)
+data.groupby('Name')['SepalWidth'].hist(bins=10, alpha=0.5, stacked=True)
+plt.title('SepalWidth', fontsize=12)
+#plt.xlabel('cm', fontsize=10)
+#plt.ylabel('Count', fontsize=10)
+plt.legend((species), loc='best', fontsize=10, fancybox=True, framealpha=0.5)
+
+plt.subplot(2,2,3)
+data.groupby('Name')['PetalLength'].hist(bins=10, alpha=0.5, stacked=True)
+plt.title('PetalLength', fontsize=12)
+plt.xlabel('cm', fontsize=10)
+plt.ylabel('Count', fontsize=10)
+plt.legend((species), loc='best', fontsize=10, fancybox=True, framealpha=0.5)
+
+plt.subplot(2,2,4)
+data.groupby('Name')['PetalWidth'].hist(bins=10, alpha=0.5, stacked=True)
+plt.title('PetalWidth', fontsize=12)
+plt.xlabel('cm', fontsize=10)
+#plt.ylabel('Count', fontsize=10)
+plt.legend((species), loc='best', fontsize=10, fancybox=True, framealpha=0.5)
+
+# To avoid title and x axis labels overlapping in the subplot.
+plt.tight_layout()
+
+plt.savefig('Hist_4attributes.jpeg')
+plt.show()
+
+quit()
 # scatter matrix?
 data.plot.scatter(x='SepalLength', y='SepalWidth')
 plt.show()
